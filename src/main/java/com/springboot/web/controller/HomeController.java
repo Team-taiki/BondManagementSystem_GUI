31a14dee986e3f1df62a3812ea646316baf59d8f
taiki <taiki@192.168.11.5>
2017-12-31 18:04:47 +0900

    [add] 取引一覧の表示
    ・TradeServiceでのgetTradelist()の実装
    ・TradeRepository, TradeDtoの追加

--- src/main/java/com/springboot/web/controller/HomeController.java
+++ src/main/java/com/springboot/web/controller/HomeController.java
@@ -2,12 +2,46 @@ package com.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/")
+	/** ホーム画面を表示
+	 *
+	 * @return home.html
+	 */
+	@RequestMapping(value="/home")
 	public String home() {
 		return "/home";
+	}
+
+	/** 取引管理画面に遷移する
+	 *
+	 * @return trade.html
+	 */
+	@RequestMapping(value="/trade", params="tradebtn", method=RequestMethod.POST)
+	public String showTradeTop() {
+		return "/trade";
+	}
+
+	@RequestMapping(value="/position", params="positionbtn", method=RequestMethod.POST)
+	public String showPositionTop() {
+		return "/position";
+	}
+
+	@RequestMapping(value="/marketvalue", params="marketvbtn", method=RequestMethod.POST)
+	public String showMarketvalueTop() {
+		return "/marketvalue";
+	}
+
+	@RequestMapping(value="/closing", params="closingbtn", method=RequestMethod.POST)
+	public String showClosingTop() {
+		return "/closing";
+	}
+
+	@RequestMapping(value="/bond", params="bondbtn", method=RequestMethod.POST)
+	public String showBondTop() {
+		return "/bond";
 	}
 }
